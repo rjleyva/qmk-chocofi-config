f4c83f78790992369650c1d0db0d9b64c91ad89f
RJ <rleyva@tutanota.com>
2025-06-17 08:55:14 -0400

    refactor(config.h): move tap_dance logic to keymap.c

--- config.h
+++ config.h
@@ -22,57 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define USE_MATRIX_I2C
 
+// Tap-Hold Configuration
 #define TAPPING_TERM 175
 #define TAPPING_TERM_PER_KEY
 
-typedef struct {
-    uint16_t tap;
-    uint16_t hold;
-    uint16_t held;
-} tap_dance_tap_hold_t;
-
-void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
-
-    if (state->pressed) {
-        if (state->count == 1
-#ifndef PERMISSIVE_HOLD
-            && !state->interrupted
-#endif
-        ) {
-            register_code16(tap_hold->hold);
-            tap_hold->held = tap_hold->hold;
-        } else {
-            register_code16(tap_hold->tap);
-            tap_hold->held = tap_hold->tap;
-        }
-    }
-}
-
-void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
-
-    if (tap_hold->held) {
-        unregister_code16(tap_hold->held);
-        tap_hold->held = 0;
-    }
-}
-
-#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
-    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, \
-      .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}) }
-
-tap_dance_action_t tap_dance_actions[] = {
-    [CT_A] = ACTION_TAP_DANCE_TAP_HOLD(KC_A, KC_LGUI),
-    [CT_R] = ACTION_TAP_DANCE_TAP_HOLD(KC_R, KC_LALT),
-    [CT_S] = ACTION_TAP_DANCE_TAP_HOLD(KC_S, KC_LCTL),
-    [CT_T] = ACTION_TAP_DANCE_TAP_HOLD(KC_T, KC_LSFT),
-    [CT_N] = ACTION_TAP_DANCE_TAP_HOLD(KC_N, KC_RSFT),
-    [CT_E] = ACTION_TAP_DANCE_TAP_HOLD(KC_E, KC_RALT),
-    [CT_I] = ACTION_TAP_DANCE_TAP_HOLD(KC_I, KC_LCTL),
-    [CT_O] = ACTION_TAP_DANCE_TAP_HOLD(KC_O, KC_RGUI),
-};
-
+// RGB lighting effects (optional, currently disabled)
 // #ifdef RGBLIGHT_ENABLE
 //     #define RGBLIGHT_EFFECT_BREATHING
 //     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
