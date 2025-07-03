eaf3fb1d92cf7e528ef4d7a24ca0bfed76e44e7c
RJ <rleyva@tutanota.com>
2025-07-03 03:29:32 -0400

    refactor(keymap): remove duplicate TAPPING_TERM definitions

--- keymap.c
+++ keymap.c
@@ -20,9 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-#define TAPPING_TERM 175
-#define TAPPING_TERM_PER_KEY
-
 // Tap Dance Structure
 typedef struct {
     uint16_t tap;
